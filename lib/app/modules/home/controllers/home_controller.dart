a52d84ba7bd7eb3d30e7d208bf69cd20cc87ea81
wangliang <liang.sl.wang@partner.bmw.com>
2023-03-27 19:47:34 +0800

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:ditredi/ditredi.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_map/plugin_api.dart';
 import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
 import '../course_gps_vector_details_model.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
@@ -11,10 +16,88 @@ class HomeController extends GetxController {
   final count = 0.obs;
   final _vectors = CourseGpsVectorDetails().obs;
   CourseGpsVectorDetails? get vectors => _vectors.value;
+
+  final _markers = <Marker>[].obs;
+  set markers(value) => _markers.value = value;
+  List<Marker> get markers => _markers;
+  List<Offset> offsets = [];
+
+  void addMarker(LatLng point, Offset offset) {
+    markers.add(
+      Marker(
+        point: point,
+        builder: (ctx) {
+          return Container(
+            color: Colors.red,
+            width: 15,
+            height: 15,
+          );
+        },
+      ),
+    );
+    offsets.add(offset);
+    generateParabola();
+    update();
+  }
+
+  void generateParabola() {
+    if (offsets.length > 1) {
+      for (var i = 0; i < offsets.length - 1; i++) {
+        Offset s = offsets[i];
+        Offset e = offsets[i + 1];
+        
+        vector.Vector3 v1 = vector.Vector3(s.dx , s.dy , 1);
+        vector.Vector3 v2 = vector.Vector3(e.dx , e.dy , 1);
+        print('xxxxxx111====$v1,$v2');
+        parabolas.add(Parabola(v1, v2, 100, 200));
+      }
+    }
+  }
+
+  Iterable<Cube3D> generateCubes() sync* {
+    final colors = [
+      Colors.grey.shade200,
+      Colors.grey.shade300,
+      Colors.grey.shade400,
+      Colors.grey.shade500,
+      Colors.grey.shade600,
+      Colors.grey.shade700,
+      Colors.grey.shade800,
+      Colors.grey.shade900,
+    ];
+
+    const count = 8;
+    for (var x = count; x > 0; x--) {
+      for (var y = count; y > 0; y--) {
+        for (var z = count; z > 0; z--) {
+          print('xxxxx====$x-$y-$z');
+          yield Cube3D(
+            0.9,
+            vector.Vector3(
+              x.toDouble() * 2,
+              y.toDouble() * 2,
+              z.toDouble() * 2,
+            ),
+            color: colors[(colors.length - y) % colors.length],
+          );
+        }
+      }
+    }
+  }
+
+  final diTreDiController = DiTreDiController(
+    rotationX: -20,
+    rotationY: 30,
+    // translation: const Offset(375/2, 0),
+    light: vector.Vector3(-0.5, -0.5, 0.5),
+  );
+
+  late MapController mapController;
   @override
   void onInit() {
     super.onInit();
     loadJson();
+    mapController = MapController();
   }
 
   void loadJson() async {
@@ -37,4 +120,37 @@ class HomeController extends GetxController {
   }
 
   void increment() => count.value++;
+  List<Parabola> parabolas = <Parabola>[];
+  Iterable<Point3D> generatePoints() sync* {
+    for (var element in parabolas) {
+      final vectors = element.getPoints();
+      for (var vector in vectors) {
+        yield Point3D(vector,color: Colors.black,width: 10);
+      }
+    }
+  }
+}
+
+class Parabola {
+  final vector.Vector3 start; // 起点
+  final vector.Vector3 end; // 终点
+  final double height; // 抛物线高度
+  final int segments; // 抛物线分段数
+
+  Parabola(this.start, this.end, this.height, this.segments);
+  List<vector.Vector3> getPoints() {
+    final List<vector.Vector3> points = [];
+    final double step = 1 / segments;
+    for (double t = 0; t <= 1; t += step) {
+      final vector.Vector3 p = vector.Vector3.zero();
+      final double x = (1 - t) * start.x + t * end.x;
+      final double y = (1 - t) * start.y + t * end.y;
+      final double z = (1 - t) * start.z + t * end.z;
+      p.setValues(x, y, z);
+      final double h = height * 4 * t * (1 - t);
+      p.y += h;
+      points.add(p);
+    }
+    return points;
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 import '../painters/map_painter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:ditredi/ditredi.dart';
+
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -9,16 +14,112 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(
       builder: (controller) {
+        Crs crs = const Epsg3857();
+        // MapController mapController = MapController();
+        var map = FlutterMap(
+          mapController: controller.mapController,
+          options: MapOptions(
+            center: LatLng(40.031841, 116.381635),
+            zoom: 5,
+            crs: crs,
+            bounds: LatLngBounds(
+              LatLng(-0.0, 0.5986328125),
+              LatLng(-0.626953125, 0.0),
+            ),
+            // swPanBoundary: LatLng(-0.626953125, 0.0),
+            // nePanBoundary: LatLng(-0.0, 0.5986328125),
+            onLongPress: (tapPosition, point) {
+              print('xxxxx===1===${tapPosition.global}===${tapPosition.relative}===');
+              controller.addMarker(point, tapPosition.relative!);
+            },
+            onMapReady: () {
+              final pixel = crs.latLngToPoint(LatLng(40.031841, 116.381635), 5);
+              print('xxxxx1====$pixel===${controller.mapController.bounds}');
+              CustomPoint<num> northWestPoint = crs.projection.project(controller.mapController.bounds!.northWest);
+              CustomPoint<num> markerPoint = crs.projection.project(LatLng(40.031841, 116.381635));
+              Offset o =
+                  Offset((markerPoint.x - northWestPoint.x).toDouble(), (northWestPoint.y - markerPoint.y).toDouble());
+              o = o.scale(1 / 2, 1 / 2);
+              print('xxxxx2====$o');
+            },
+          ),
+          nonRotatedChildren: [
+            AttributionWidget.defaultWidget(
+              source: 'OpenStreetMap contributors',
+              onSourceTapped: () {},
+            ),
+          ],
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.liang.iskylab',
+            ),
+            MarkerLayer(markers: controller.markers),
+          ],
+        );
         return Scaffold(
           appBar: AppBar(
             title: const Text('HomeView'),
             centerTitle: true,
           ),
-          body: Center(
-            child: CustomPaint(
-              size: const Size(500, 400),
-              painter: MapPainter(vectors: controller.vectors?.vectorGPSObject?.vegetation),
-            ),
+          body: Stack(
+            children: [
+              Transform(
+                transform: Matrix4(
+                  1,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                )
+                  ..setEntry(3, 2, 0.001)
+                  ..rotateX(0)
+                  ..rotateY(0.0)
+                  ..rotateZ(0.0),
+                alignment: FractionalOffset.center,
+                child: map,
+              ),
+              // IgnorePointer(
+              //   child: DiTreDiDraggable(
+              //     controller: controller.diTreDiController,
+              //     scaleEnabled: false,
+              //     child: Container(
+              //       width: 300,
+              //       height: 300,
+              //       color: Colors.grey,
+              //       child: DiTreDi(
+              //         figures: [
+              //           Point3D(vector.Vector3(0, 0, 0), color: Colors.purple, width: 5),
+              //           Point3D(vector.Vector3(0, 1, 0), color: Colors.green, width: 5),
+              //           Point3D(vector.Vector3(0, 2, 0), color: Colors.green, width: 5),
+              //         ],
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              if (controller.parabolas.length > 6)
+                IgnorePointer(
+                  // ignoring: controller.markers.length > 8,
+                  child: DiTreDiDraggable(
+                    controller: controller.diTreDiController,
+                    child: DiTreDi(
+                      figures: controller.generatePoints().toList(),
+                      controller: controller.diTreDiController,
+                    ),
+                  ),
+                ),
+            ],
           ),
         );
       },
